ff3009914eae0815abbf6508fee5002646e75ae8
cristian-picchio <72785153+cristian-picchio@users.noreply.github.com>
2020-10-17 09:47:39 +0200

    Add files via upload

---
+++ credito residuo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CreditoResiduo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double credito, spesa, contspesa = 0, spesadafare;
+            Console.WriteLine("inserisci il tuo saldo iniziale);
+            credito = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("inserisci il numero di spese che vuoi effettuare");
+            spesadafare = Convert.ToDouble(Console.ReadLine());
+            while (contspesa < spesadafare) ;
+            {
+                Console.WriteLine("inserisci l'importo della spesa");
+                spesa = Convert.ToDouble(Console.ReadLine());
+                if (spesa > credito) ;
+                {
+                    Console.WriteLine("LA TUA SPESA E' MAGGIORE DEL CREDITO TOTALE");
+                    break;
+                }
+                else
+                {
+                    credito = credito - spesa;
+                    Console.WriteLine($"il credito rimasto dopo la tua spesa è {credito}");
+                }
+                contspesa++;
+            }
+            Console.WriteLine($"hai eseguito {contspesa} spese, e il credito finale è {credito}");
+            
+        }
+    }
+}
