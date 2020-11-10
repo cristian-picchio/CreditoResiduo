3ea823f96c11d3b75c8426a371192ba55e50003a
cristian-picchio <72785153+cristian-picchio@users.noreply.github.com>
2020-11-10 20:52:18 +0100

    Add files via upload

---
+++ convertitoreASCII.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace esconvertitoreascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "D";
+            {
+                // get the integral value of character.
+                int value = Convert.ToInt32(letter);
+                // convert the decimal value to a hexadecimal value in string form.
+
+                Console.WriteLine("hexadecimal value of {0} is {1}", letter, value);
+            }
+
+        }
+    }
+}
